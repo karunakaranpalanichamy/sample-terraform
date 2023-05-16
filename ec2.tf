97fe6cbde38f9db317ce80c33602dc8fab4fb1a2
kpalanichamy <karunakaran.palanichamy@changehealthcare.com>
2023-05-16 15:33:44 -0400

    removed the secret key

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAYNGUIG3FWEH5C7UM"
-  secret_key = "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"
+  
 }
 
 resource "aws_instance" "myec2" {
